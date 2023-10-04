51e0d227ca53706a59157d190623062d6568aa84
Umut Ozdemir <umutozdemir3555@gmail.com>
2023-10-04 19:02:29 +0300

    Second Part has changed and Test image reuploaded.

--- Exercise_1/histogramEqualization.py
+++ Exercise_1/histogramEqualization.py
@@ -12,21 +12,24 @@ MAX_PIXEL_VALUE = 255
 print(image.shape)
 
 histogram_values = np.zeros(256)
+LOWEST_PIXEL_VALUE = 255
 
 for i in range(m):
     for j in range(n):
+        if image[i][j]<LOWEST_PIXEL_VALUE:
+            LOWEST_PIXEL_VALUE = image[i][j]
         histogram_values[image[i][j]] += 1
 
 histogram_distribution = np.zeros(256)
-
+print(LOWEST_PIXEL_VALUE)
 for i in range(m):
     histogram_distribution[i]=histogram_values[i]/TOTAL_PIXEL
 
-temp_total_distribution = 0.0
+total_distribution_cumulative_1 = 0.0
 histogram_equalization_pixel_matching = np.zeros(256)
 for i in range(m):
-    temp_total_distribution+=histogram_distribution[i]
-    temp_value = temp_total_distribution*MAX_PIXEL_VALUE
+    total_distribution_cumulative_1+=histogram_distribution[i]
+    temp_value = total_distribution_cumulative_1*MAX_PIXEL_VALUE
     histogram_equalization_pixel_matching[i] = math.floor(temp_value+0.5)
 
 new_image = image.copy()
@@ -35,7 +38,6 @@ for i in range(m):
         new_image[i][j] = histogram_equalization_pixel_matching[new_image[i][j]]
 
 cv.imshow('Output_1', new_image)
-
 
 equ_opencv = cv.equalizeHist(image)
 
@@ -48,28 +50,23 @@ for i in range(m):
 cv.imshow('Difference', difference_image)
 
 # SECOND PART
-histogram_values_2 = np.zeros(256)
-lowest_pixel_value = 255
-for i in range(m):
-    for j in range(n):
-        if image[i][j]<lowest_pixel_value:
-            lowest_pixel_value = image[i][j]
-        histogram_values_2[image[i][j]] += 1
+# We will use histogram_values as we calculated before
 histogram_values_2_cumulative = np.zeros(256)
-histogram_values_2_cumulative[0] = histogram_values_2[0]
+histogram_values_2_cumulative[0] = histogram_values[0]
 for i  in range(1,256):
-    histogram_values_2_cumulative[i] = histogram_values_2[i]+histogram_values_2_cumulative[i-1]
+    histogram_values_2_cumulative[i] = histogram_values[i]+histogram_values_2_cumulative[i-1]
 
 histogram_equalization_pixel_matching_2 = np.zeros(256)
-Hmin=histogram_values_2_cumulative[lowest_pixel_value]
+Hmin=histogram_values_2_cumulative[LOWEST_PIXEL_VALUE]
 for i in range(256):
-        temp1 = histogram_values_2_cumulative[i]-Hmin
-        temp2 = m*n-Hmin
-        histogram_equalization_pixel_matching_2[i] = round((temp1/temp2)*255)
+    dividend = histogram_values_2_cumulative[i]-Hmin
+    divisor = m*n-Hmin
+    histogram_equalization_pixel_matching_2[i] = round((dividend/divisor)*(MAX_PIXEL_VALUE))
+        
 new_image_2 = image.copy()
 for i in range(m):
     for j in range(n):
-        new_image_2[i][j] = histogram_equalization_pixel_matching_2[new_image[i][j]]
+        new_image_2[i][j] = histogram_equalization_pixel_matching_2[new_image_2[i][j]]
 
 cv.imshow('Output 3', new_image_2)
 difference_image_2 = image.copy()
@@ -79,6 +76,10 @@ for i in range(m):
         difference_image_2[i][j] = abs(int(new_image_2[i][j]) - int(equ_opencv[i][j]))
 cv.imshow('Difference2', difference_image_2)
 print(difference_image_2)
+for i in range(256):
+    for j in range(256):
+        if difference_image_2[i][j] != 0:
+            print("HATA")
 cv.waitKey(0)
 
 cv.destroyAllWindows()
