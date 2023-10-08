118d961d33a7cc0529cf4a70f0edfeb0af821cc9
DoyleBellamy <umutozdemir3555@gmail.com>
2023-10-08 22:00:55 +0300

    Exercise_2_Q1 çözüm

--- Exercise_2/boxFilter.py
+++ Exercise_2/boxFilter.py
@@ -6,7 +6,7 @@ image = cv.imread(os.path.join(os.getcwd(),'lena_grayscale_hq.jpg'),cv.IMREAD_GR
 
 m,n = image.shape
 
-def box_filter_function(m, n, box_filter_size):
+def box_filter_function(m, n, image,box_filter_size):
     #Box filter size User Input Part
 
     # box_filter_size = 0
@@ -46,8 +46,8 @@ def box_filter_function(m, n, box_filter_size):
     return box_filter_image
 
 def openCVBoxFilter(box_filter_size):
-    kernel = np.ones((3,3),np.float32)/(3**2)
-    padding_size = 1
+    kernel = np.ones((box_filter_size,box_filter_size),np.float32)/(box_filter_size**2)
+    padding_size = int(box_filter_size/2)
     image_padded = cv.copyMakeBorder(image,padding_size,padding_size,padding_size,padding_size,cv.BORDER_CONSTANT,value = 0)
     dst = cv.filter2D(image_padded,-1,kernel)
     result_1_1 = dst[padding_size:-padding_size,padding_size:-padding_size]
@@ -64,14 +64,32 @@ def getDifferenceImage(m,n,image1,image2):
     return difference_image_1_2
 
 cv.imshow('Input image',image)
-image_3_3 = box_filter_function(m,n,3)
+
+# 3x3 Filter
+box_filter_size = 3
+image_3_3 = box_filter_function(m,n,image,box_filter_size)
 cv.imshow('output1_1', image_3_3)
+image_3_3_openCv = openCVBoxFilter(box_filter_size)
+cv.imshow('output1_2', image_3_3_openCv)
+differenceImage_1_2_1 = getDifferenceImage(m,n,image_3_3,image_3_3_openCv)
+cv.imshow('Difference_1_2_1', differenceImage_1_2_1)
 
-image_3_3_openCv = openCVBoxFilter(3)
-cv.imshow('output2_1', image_3_3_openCv)
+# 11x11 Filter
+box_filter_size = 11
+image_11_11 = box_filter_function(m,n,image,box_filter_size)
+cv.imshow('output2_1', image_11_11)
+image_11_11_openCv = openCVBoxFilter(box_filter_size)
+cv.imshow('output2_2', image_11_11_openCv)
+differenceImage_1_2_2 = getDifferenceImage(m,n,image_11_11,image_11_11_openCv)
+cv.imshow('Difference_1_2_2', differenceImage_1_2_2)
 
-differenceImage_1_2 = getDifferenceImage(m,n,image_3_3,image_3_3_openCv)
-
-cv.imshow('Difference', differenceImage_1_2)
+# 21x21 Filter
+box_filter_size = 21
+image_21_21 = box_filter_function(m,n,image,box_filter_size)
+cv.imshow('output3_1', image_21_21)
+image_21_21_openCv = openCVBoxFilter(box_filter_size)
+cv.imshow('output3_2', image_21_21_openCv)
+differenceImage_1_2_3 = getDifferenceImage(m,n,image_21_21,image_21_21_openCv)
+cv.imshow('Difference_1_2_3', differenceImage_1_2_3)
 
 cv.waitKey(0)
